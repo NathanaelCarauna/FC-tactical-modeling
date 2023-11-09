bb3ded74734469d3e9fa2091e62a9977dab7acdc
nathanael <nathanael.caruana@gmail.com>
2023-11-09 13:14:04 -0300

    entity as should be using DDD, it uses autovalidation, focus in exprecivity

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,36 +1,41 @@
 //Entidade anêmica, apenas armazena dados. Usada normalmente para ORMs
+//Uma entidade sempre deve se autovalidar
+//Devemos separar o que é complexidade de negócio e complexidade acidental
 class Customer {
     private _id: string;
     private _name: string;
-    private _address: string;
+    private _address: string = "";
+    private _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
     }
 
-    public get id(): string {
-        return this._id;
+    validate(){
+        if(this._name.length === 0){
+            throw new Error("Name is required");            
+        }
+        if(this._id.length === 0){
+            throw new Error("Id is required");
+        }
     }
 
-    public set id(value: string) {
-        this._id = value;
+    changeName(name: string){
+        this._name = name;
+        this.validate();
     }
 
-    public get name(): string {
-        return this._name;
+    activate(){
+        if(this._address.length === 0){
+            throw new Error("Address is mandatory to activate a customer");
+        }
+        this._active = true;
     }
 
-    public set name(value: string) {
-        this._name = value;
+    deactivate(){
+        this._active = false;
     }
 
-    public get address(): string {
-        return this._address;
-    }
-
-    public set address(value: string) {
-        this._address = value;
-    }
 }
