f38d98061c941e43e882b13572543b43961030e6
nathanael <nathanael.caruana@gmail.com>
2023-11-09 14:09:16 -0300

    create address VO

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,10 +1,13 @@
 //Entidade anêmica, apenas armazena dados. Usada normalmente para ORMs
 //Uma entidade sempre deve se autovalidar
+
+import Address from "./address";
+
 //Devemos separar o que é complexidade de negócio e complexidade acidental
 class Customer {
     private _id: string;
     private _name: string;
-    private _address: string = "";
+    private _address!: Address;
     private _active: boolean = false;
 
     constructor(id: string, name: string) {
@@ -28,7 +31,7 @@ class Customer {
     }
 
     activate(){
-        if(this._address.length === 0){
+        if(this._address !== undefined){
             throw new Error("Address is mandatory to activate a customer");
         }
         this._active = true;
@@ -36,6 +39,10 @@ class Customer {
 
     deactivate(){
         this._active = false;
+    }
+
+    set Address(address: Address){
+        this._address = address;
     }
 
 }
