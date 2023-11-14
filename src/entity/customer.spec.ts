af46ffe33d97e339b8d69d4e80b726d80e1f69bd
nathanael <nathanael.caruana@gmail.com>
2023-11-14 12:09:27 -0300

    cover customer entity with tests

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -1,6 +1,61 @@
+import Address from "./address";
+import Customer from "./customer";
+
 describe("Customer unit tests", () => {
-    it("should get 1 as a result", () => {
-        const result =1;
-        expect(result).toBe(1);
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let customer = new Customer("", "John");
+        }).toThrow("Id is required");
+    });
+
+    it("should throw error when name is empty", () => {
+        expect(() => {
+            let customer = new Customer("123", "");
+        }).toThrow("Name is required");
+    });
+
+    it("should change name", () => {
+        //arrange
+        const customer = new Customer("123", "John");
+
+        //Act
+        customer.changeName("Jane");
+
+        //Assert
+        expect(customer.name).toBe("Jane");
+    });
+
+    it("should activate customer", () => {
+        //arrange
+        const customer = new Customer("1", "Customer1");
+        const address = new Address("Street 1", 123, "13330-250", "São paulo");
+        customer.Address = address;
+
+        //Act
+        customer.activate()
+
+        //Assert
+        expect(customer.isActive()).toBe(true);
+    });
+
+    it("should throw error when address is undefined and customer is activated", () => {
+        expect(() => {
+            //arrange
+            const customer = new Customer("1", "Customer1");
+            //Act
+            customer.activate()
+            //Assert
+        }).toThrow("Address is mandatory to activate a customer")
+    });
+
+    it("should deactivate customer", () => {
+        //arrange
+        const customer = new Customer("1", "Customer1");
+
+        //Act
+        customer.deactivate()
+
+        //Assert
+        expect(customer.isActive()).toBe(false);
     });
 });
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -14,34 +14,42 @@ export default class Customer {
         this._id = id;
         this._name = name;
         this.validate();
+    }    
+
+    get name(): string {
+        return this._name;
     }
 
-    validate(){
-        if(this._name.length === 0){
-            throw new Error("Name is required");            
-        }
-        if(this._id.length === 0){
+    isActive(): boolean {
+        return this._active;
+    }
+
+    validate() {
+        if (this._id.length === 0) {
             throw new Error("Id is required");
         }
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
     }
 
-    changeName(name: string){
+    changeName(name: string) {
         this._name = name;
         this.validate();
     }
 
-    activate(){
-        if(this._address !== undefined){
+    activate() {
+        if (this._address === undefined) {
             throw new Error("Address is mandatory to activate a customer");
         }
         this._active = true;
     }
 
-    deactivate(){
+    deactivate() {
         this._active = false;
     }
 
-    set Address(address: Address){
+    set Address(address: Address) {
         this._address = address;
     }
 
