6d3f3f5e558f0675c32806c0922e7ae53d62ea68
nathanael <nathanael.caruana@gmail.com>
2023-11-14 12:49:39 -0300

    cover order with tests

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,16 +1,31 @@
 import OrderItem from "./order_item";
 
 export default class Order{
-    _id: string;
-    _custumerId: string;
-    _items: OrderItem[];
+    private _id: string;
+    private _custumerId: string;
+    private _items: OrderItem[];
+    private _total: number;
 
     constructor(id: string, custumerId: string, items: OrderItem[]){
         this._id = id;
         this._custumerId = custumerId;
         this._items = items;
+        this._total = this.total();
+        this.validate();
     }
 
+    validate(): boolean{
+        if(this._id.length === 0){
+            throw new Error("Id is required");
+        }
+        if(this._custumerId.length === 0){
+            throw new Error("CustomerId is required");
+        }
+        if(this._items.length === 0){
+            throw new Error("Items are required");
+        }
+        return true;
+    }
     total(): number {
         return this._items.reduce((acc, item) => acc + item._price, 0);
     }
