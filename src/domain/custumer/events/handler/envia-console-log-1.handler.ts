9c11286bb9fe94aa589083afbde6b686e835113c
nathanael <nathanael.caruana@gmail.com>
2023-11-21 10:19:16 -0300

    fix: include expected event type on customer eventHandlers

--- src/domain/custumer/events/handler/envia-console-log-1.handler.ts
+++ src/domain/custumer/events/handler/envia-console-log-1.handler.ts
@@ -1,8 +1,8 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
-import EventInterface from "../../../@shared/event/event.interface";
+import CustomerCreatedEvent from "../customer-created";
 
 export default class EnviaConsoleLog1Handler implements EventHandlerInterface {
-    handle(event: EventInterface): void {
+    handle(event: CustomerCreatedEvent): void {
         console.log("Esse é o primeiro console.log do evento: CustomerCreated");
     }
 }
--- src/domain/custumer/events/handler/envia-console-log-handler.ts
+++ src/domain/custumer/events/handler/envia-console-log-handler.ts
@@ -1,8 +1,9 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
-import EventInterface from "../../../@shared/event/event.interface";
+import CustomerAddressChangedEvent from "../customer-address-changed";
+import CustomerCreatedEvent from "../customer-created";
 
 export default class EnviaConsoleLogHandler implements EventHandlerInterface{
-    handle(event: EventInterface): void {
+    handle(event: CustomerAddressChangedEvent): void {
         console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para ${event.eventData.address}`);
     }
 }
