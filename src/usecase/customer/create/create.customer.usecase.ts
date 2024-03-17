35d521bbad14056dbec71dea113de24d017e4376
Nathanael Caraúna <nathanael.carauna@gmail.com>
2024-03-17 11:03:58 -0300

    feat: implement create customer use case, with tests

---
+++ src/usecase/customer/create/create.customer.usecase.ts
@@ -0,0 +1,28 @@
+import CustomerFactory from "../../../domain/custumer/factory/customer.factory";
+import CustomerRepositoryInterface from "../../../domain/custumer/repository/customer-repository.interface";
+import Address from "../../../domain/custumer/value-object/address";
+import { InputCreateCustomerDto, OutputCreateCustomerDto } from "./create.customer.dto";
+
+export default class CreateCustomerUseCase {
+    private customerRepository: CustomerRepositoryInterface;
+
+    constructor(customerRepository: CustomerRepositoryInterface) {
+        this.customerRepository = customerRepository;
+    }
+
+    async execute(input: InputCreateCustomerDto): Promise<OutputCreateCustomerDto> {
+        const customer = CustomerFactory.createWithAddress(input.name,
+            new Address(input.address.street, input.address.number, input.address.zip, input.address.zip));
+        await this.customerRepository.create(customer);
+        return {
+            id: customer.id,
+            name: customer.name,
+            address: {
+                street: customer.address.street,
+                number: customer.address.number,
+                zip: customer.address.zipcode,
+                city: customer.address.city,
+            }
+        }
+    }
+}
