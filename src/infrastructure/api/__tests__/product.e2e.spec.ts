ccc6f9af88f6b2e2a61265bbde9010b295509201
Nathanael Caraúna <nathanael.carauna@gmail.com>
2024-04-08 08:47:07 -0300

    test: test name and invalid price at product creation endpoint

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -23,4 +23,15 @@ describe("E2E test for product", () => {
         expect(response.body.name).toBe("Product 1");
         expect(response.body.price).toBe(99);
     })
+
+    it("Should not create a product", async () => {
+        const response = await request(app)
+            .post('/product')
+            .send({
+                name: "",
+                price: 0,
+                type: 'a'
+            })
+        expect(response.status).toBe(500);        
+    })
 })
